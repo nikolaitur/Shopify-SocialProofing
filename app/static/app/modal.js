b8bc40005b5d0fe03b10866b43b20f3bb6250d60
nikolaitur <n.turovskyi@gmail.com>
2017-11-08 18:47:26 -0500

    appending script to head

--- app/static/app/modal.js
+++ app/static/app/modal.js
@@ -1,9 +1,16 @@
-  console.log("Enter this block.");
-  alert("Hey youre here");
-  var para = document.createElement("p");
-  var node = document.createTextNode("This is new.");
+  (function () {
+  	//Build a pseudo-class to prevent polluting our own scope.
+    console.log("Enter this block.");
+    alert("Hey youre here");
+    var para = document.createElement("p");
+    var node = document.createTextNode("This is new.");
 
-  para.appendChild(node);
+    para.appendChild(node);
 
-  var element = document.getElementById("MainContent");
-  element.appendChild(para);
+    var element = document.getElementById("MainContent");
+    element.appendChild(para);
+    
+    var script = document.createElement("script");
+    script.src = "https://protected-reef-37693.herokuapp.com/app/static/js/modal.js";
+    document.head.appendChild(script);
+  }());
