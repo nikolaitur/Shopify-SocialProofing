5154576d312d932c52049f8c16babfce0bd46654
nikolaitur <n.turovskyi@gmail.com>
2017-11-12 15:43:23 -0500

    commenting out staticfiles storage for temporary fix

--- app/settings.py
+++ app/settings.py
@@ -132,7 +132,8 @@ STATICFILES_DIRS = [
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Commenting out until bundle has good integration.
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Logging settings
 
